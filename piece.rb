58a7beae3dab8c0f642d2eaa602848e24ef8dc48
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-04 14:36:46 -0700

    Implement SteppingPiece module

--- piece.rb
+++ piece.rb
@@ -1,14 +1,24 @@
-require_relative "null_piece"
+# require_relative "null_piece" TODO comment back ing
 
 class Piece
-
+  attr_reader :color
+  
   def initialize(position, board, color)
     @color = color
     @position = position
     @board = board
   end
 
-  def moves
+  def to_s
   end
+
+  def empty?
+
+  end
+
+  def move_into_check
+
+  end
+
 
 end
--- stepping_piece.rb
+++ stepping_piece.rb
@@ -1,2 +1,49 @@
+require 'byebug'
+
 module SteppingPiece
+
+  SPACE_MOVES = [
+    [1,0],
+    [1,1],
+    [1,-1],
+    [-1, 0],
+    [-1, -1],
+    [-1, 1],
+    [0, 1],
+    [0, -1]
+  ]
+
+  L_MOVES = [
+    [1, 2],
+    [1, -2],
+    [-1, 2],
+    [-1, -2],
+    [2, 1],
+    [2, -1],
+    [-2, 1],
+    [-2, -1]
+  ]
+
+  def moves(start_pos)
+
+    all_moves = move_diffs.map do |el|
+      [el[0] + start_pos[0],
+      el[1] + start_pos[1]]
+    end
+
+    all_moves.reject! do |move|
+      @board[move].color == @color
+    end
+
+    # remove out of bounds stuff (which shouldn't matter;
+    # cursor won't allow user to even go out of bounds
+    all_moves.select do |move|
+      move.first.between?(0, 7) && move.last.between?(0, 7)
+    end
+  end
+
+  private
+
+  def move_diffs
+  end
 end
